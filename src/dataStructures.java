7d908315c7c014e3e14866ea51f3a147b0533f05
M. Kashwah <mkashwah@ryerson.ca>
2020-12-21 03:26:24 -0500

    another hello statement

--- src/dataStructures.java
+++ src/dataStructures.java
@@ -1,6 +1,6 @@
 public class dataStructures {
     public static void main(String[] args){
         System.out.println("Hello world!!");
-        
+        System.out.println("another hello statement");
     }
 }
