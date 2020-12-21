cb4b0e7997f36dc2541683f82963c71da22286a8
M. Kashwah <mkashwah@ryerson.ca>
2020-12-21 07:33:49 -0500

    change main folder

--- misc/src/dataStructures.java
+++ misc/src/dataStructures.java
@@ -2,7 +2,7 @@ public class dataStructures {
     public static void main(String[] args){
          String[] words = {"hello", " ", "my name is ", "Slim Shady"};
          //memory for 4 * 64 bytes is allocated for the 4 items we created in words[]
-        
+
 
     }
 }
