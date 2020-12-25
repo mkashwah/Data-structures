def2bc1d033c8e0e10ac138a040d0a0d2c73eae8
M. Kashwah <mkashwah@ryerson.ca>
2020-12-24 21:15:44 -0500

    fixed constructor issue

--- dynamicArrays/src/dynamicArrays.java
+++ dynamicArrays/src/dynamicArrays.java
@@ -1,13 +1,41 @@
 import java.util.Arrays;
 public class dynamicArrays extends reverseString {
-    private int length;
-    private int[] data = {};
+    private int len;
+    int[] data;
+
+    //The following functions are needed to work with an array
+    //for this example i will be using an array of integers
+    //the functions we need are as follows:
+    //push(int value)
+    //pop()
+    //insert(int index, int value)
+    //delete(int index)
+    //shiftItems(int index)
+    //get(int index)
 
     //constructor
-    public void dynamicArrays(){
-        int[] data = new int[2];
-        length = 0;
+    public dynamicArrays(){
+        len = 0;
+        data = new int[0];
+        System.out.println("array created");
     }
+
+    //get(int index)
+    public int get(int index){
+        return this.data[index];
+    }
+
+    //push(int value)
+    public void push(int val){
+        int[] temp = new int[data.length+1];
+        for(int i = 0; i <= data.length; i++){
+            temp[i]= data[i];
+        }
+        temp[data.length + 1] = val;
+        data = temp;
+        len++;
+    }
+
 
 
     public static void main(String[] args){
@@ -17,17 +45,17 @@ public class dynamicArrays extends reverseString {
         //////////////////////////////
 
 
-        
+
 
 
         dynamicArrays arr1 = new dynamicArrays();
-        arr1.length = 1;
-        int dat[] = new int[2];
-        dat[0] = 10;
-//        arr1.data[0] = 5;
-//        arr1.data[1] = 10;
-//        System.out.println(arr1.data.length);
-//        System.out.println(dat.length);
+        System.out.println(arr1.data.length);
+//        System.out.println(arr1.data);
+        //arr1.push(5);
+
+
+        //System.out.println(arr1.get(0));
+
 
 
     }
